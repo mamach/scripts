6a70c26b2b4f546d4421e4b2e0dc289377c0a6d9
Mahesh Chandra Maheswaram <13414@cloud4c.local>
2022-12-21 15:58:36 +0530

    first commit

---
+++ myscript.bat
@@ -0,0 +1,13 @@
+@REM type the file name in terminal to execute
+doskey ls=dir
+doskey ll=dir /w
+doskey cdrepos=cd %userprofile%/Desktop/Projects/repos
+doskey cdscripts=cd %userprofile%/Desktop/Projects/scripts
+doskey pwd=cd
+doskey lsdoskey=doskey /macros
+doskey cat=type $*
+doskey nano=notepad $*
+doskey touch=notepad $*
+doskey grep=findstr $*
+doskey reboot=shutdown /r
+doskey rm=del $*
